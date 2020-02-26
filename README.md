14eee9574a5ae7ad58a416e6a97d4ad590e1fcc1
inroman <roman.pikov@gmail.com>
2020-02-27 00:25:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-3cuwip
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-3cuwip)
---
+++ src/app/shap/shap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shap',
+  templateUrl: './shap.component.html',
+  styleUrls: ['./shap.component.css']
+})
+export class ShapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
